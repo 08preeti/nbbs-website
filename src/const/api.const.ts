3e5e59fda8c5dac2b9cd9334f8b828f1fa45b9e0
SachinPandit13 <sachininfo13@gmail.com>
2026-09-03 16:39:52 +0530

    added ceredebility endpoint const

--- src/const/api.const.ts
+++ src/const/api.const.ts
@@ -1,2 +1,3 @@
 export const INDUSTRIES_ENDPOINT = "/api/nbbs/industries/";
 export const ANCHOR_CUSTOMERS_ENDPOINT = "/api/nbbs/anchor-customers/";
+export const CREDIBILITY_ENDPOINT = "/api/nbbs/credibility/";
